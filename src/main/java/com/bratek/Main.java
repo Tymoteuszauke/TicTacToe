595ed111810921b1af2741d43a2df8f53bb3db5e
Mateusz <mhbratek@gmail.com>
2017-06-28 12:32:00 +0200

    To do communication with

---
+++ src/main/java/com/bratek/Main.java
@@ -0,0 +1,16 @@
+package com.bratek;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+/**
+ * Created by bratek on 28.06.17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        //TODO: Prepare bi-lingual impl of communication with players.
+        System.out.println("Tell me, which character will start first: ");
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
