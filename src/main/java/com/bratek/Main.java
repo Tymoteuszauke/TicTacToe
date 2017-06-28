3d9cbfff2895f3bf7e34b7a2070f69afdf5e83f8
Mateusz <mhbratek@gmail.com>
2017-06-28 12:37:57 +0200

    Implementation of communication with human must be done

--- src/main/java/com/bratek/Main.java
+++ src/main/java/com/bratek/Main.java
@@ -1,6 +1,6 @@
 package com.bratek;
 
-import java.io.InputStream;
+
 import java.util.Scanner;
 
 /**
@@ -8,9 +8,14 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args) {
+        //TODO: Prepare impl of game with Human or game with Computer
+        System.out.println("Human vs Human (1) \nHuman vs Computer(2) \nComputer vs Computer (3)");
+
         //TODO: Prepare bi-lingual impl of communication with players.
         System.out.println("Tell me, which character will start first: ");
         Scanner scanner = new Scanner(System.in);
-        
+
+        //TODO: Prepare impl of User, contains his sign.
+        String userSign = scanner.nextLine();
     }
 }
